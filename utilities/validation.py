02c0178a5218035ca7bff91619b9cef15531fd81
Maksymilian Sekula <maksymilian.sekula@hotmail.com>
2024-01-30 17:12:47 +0000

    Refactor validation code to separate file

---
+++ utilities/validation.py
@@ -0,0 +1,6 @@
+"""Methods to validate flag combinations/options are correct"""
+
+def validate_flag_choices(flag_choice: dict[str, bool]) -> dict[str, bool]:
+    del flag_choice["-fkeep-inline-dllexport"]  # Not supported by my configuration
+    flag_choice["-fsection-anchors"] = flag_choice["-ftoplevel-reorder"] = flag_choice["-funit-at-a-time"] # Needs to be the same as the others
+    return flag_choice
