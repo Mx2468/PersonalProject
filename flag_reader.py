671a4c34e3b0dbf64ebfd28adc5a51f64a76d87a
Maksymilian Sekula <maksymilian.sekula@hotmail.com>
2024-01-30 20:18:47 +0000

    remove flag reader test script

--- flag_reader.py
+++
@@ -1,8 +0,0 @@
-# Module to read in flags for the compiler to use
-from os import read
-
-def read_binary_flags() -> list[str]:
-    """Read binary flags from the known location"""
-    with open("binary_flags.txt", "r") as flagfile:
-        flags = [line.strip() for line in flagfile]
-        return flags
