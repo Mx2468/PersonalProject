49c26dbcacbdf335506b221b0394590c7e4ae2b3
Maksymilian Sekula <maksymilian.sekula@hotmail.com>
2024-01-30 22:00:46 +0000

    add improvements to make generic flag file reader work

--- reader/flag_file_reader.py
+++ reader/flag_file_reader.py
@@ -1,5 +1,6 @@
 """An abstract class of readers for flag files"""
 from abc import ABC, abstractmethod
+
 class FlagFileReader(ABC):
     """An abstract class of readers for flag files"""
     def __init__(self, file_name: str):
@@ -8,20 +9,25 @@ class FlagFileReader(ABC):
         """
         self.file = None
         self.file_name = file_name
+        self.flags = None
 
-    def __enter__(self) -> None:
+    def __enter__(self) -> 'FlagFileReader':
         """Overwrites magic method to allow easy use with "with" statements"""
         self.file = open(self.file_name, 'r', encoding='UTF-8')
+        return self
 
     def __exit__(self, exception_type, exception_value, traceback) -> None:
         """Uses magic method to close the file after we're done with it in a "with" statement"""
         self.file.close()
 
     @abstractmethod
-    def read_in_flags(self) -> list[str]:
+    def read_in_flags(self) -> None:
         """
         Reads flags from the opened file and returns the read flags as a list
-
         :return: List of strings containing the read flags
         """
         raise NotImplementedError
+
+    def get_flags(self) -> None:
+        """Return flags that have been read in"""
+        return self.flags
