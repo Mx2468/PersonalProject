60f53cc7483ffb8d5ee8bc76e4e531d4ad5a3778
Maksymilian Sekula <maksymilian.sekula@hotmail.com>
2024-02-26 19:10:56 +0000

    Implement domain flags reader

--- reader/domain_flag_reader.py
+++ reader/domain_flag_reader.py
@@ -2,12 +2,34 @@ import json
 from reader.flag_file_reader import FlagFileReader
 
 class DomainFlagReader(FlagFileReader):
+    all_names: list[str]
+    domains: dict[str, str]
+    defaults: dict[str, str]
     def __init__(self, file_name: str):
         super().__init__(file_name)
 
     def read_in_flags(self) -> None:
-        """Read domain flags"""
-        print(json.load(self.file))
+        """Read the domain flags, their domains, and the default choices"""
+        loaded_data = json.load(self.file)
         # Identify each name
-        # Extract domains
-        # combine both into flags info self.flags
+        all_flags = loaded_data["flags"]
+        self.all_names = [flag["flagname"] for flag in all_flags]
+
+        for flag in all_flags:
+            # Extract domains
+            if flag["domaintype"] == "Choice":
+                self.domains[flag["flagname"]] = flag["choices"]
+            else:
+                self.domains[flag["flagname"]] = flag["domaintype"]
+
+        # Get default choices
+        self.defaults = {flag["flagname"]: flag["default"] for flag in all_flags}
+
+    def get_flags(self) -> list[str]:
+        return self.all_names
+
+    def get_domains(self) -> dict[str, str]:
+        return self.domains
+
+    def get_default_values(self) -> dict[str, str]:
+        return self.defaults
