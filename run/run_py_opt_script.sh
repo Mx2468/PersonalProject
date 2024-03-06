064b667fc57d279a89021dab8a9ed5195262b10a
Maksymilian Sekula <maksymilian.sekula@hotmail.com>
2024-03-06 21:30:19 +0000

    Start adding flags to run script

--- run/run_py_opt_script.sh
+++ run/run_py_opt_script.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-conda run --live-stream -n PersonalProject python flag_controller.py
+conda run --live-stream -n PersonalProject python flag_controller.py -i ./test_cases/primes.cpp
