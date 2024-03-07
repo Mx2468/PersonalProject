3c0c6d53adc8aea6e2779bfbb89d2bdabaa95737
Maksymilian Sekula <maksymilian.sekula@hotmail.com>
2024-03-07 20:35:44 +0000

    Start to introduce run scripts with correct flags

---
+++ run/run_genetic_continuous_optimisation.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env bash
+conda run --live-stream -n PersonalProject python flag_controller.py -i ./test_cases/primes.cpp --method genetic
--- run/run_py_opt_script.sh
+++
@@ -1,2 +0,0 @@
-#!/usr/bin/env bash
-conda run --live-stream -n PersonalProject python flag_controller.py -i ./test_cases/primes.cpp
