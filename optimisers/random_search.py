d248ae4a0b7023bca8fd37fba5c1234c1f39fa66
Maksymilian Sekula <maksymilian.sekula@hotmail.com>
2024-03-08 13:53:41 +0000

    Adapt random search to fit new flag object

--- optimisers/random_search.py
+++ optimisers/random_search.py
@@ -1,35 +1,24 @@
 """ A class used to implement a random search algorithm for optimising flags """
 import helpers
+from core.flags import Flags
 from optimisers.optimiser import FlagOptimiser
 from helpers import validate_flag_choices, create_flag_string, Benchmarker
 
 
 class RandomSearchOptimiser(FlagOptimiser):
     """ A class for random search optimisation of compiler flags"""
-    def __init__(self, flags_to_optimise: list[str]):
-        super().__init__(flags_to_optimise)
 
-    def optimisation_step(self, flags: dict[str, bool]) -> dict[str, bool]:
-        return validate_flag_choices(helpers.get_random_flag_sample(list(flags.keys())))
+    def __init__(self, flags_to_optimise: Flags):
+        super().__init__(flags_to_optimise)
+        self.flags_object = flags_to_optimise
+
+    def get_random_flags(self, flags: dict[str, bool]) -> dict[str, bool]:
+        return validate_flag_choices(helpers.get_random_flag_sample(self.flags_object))
 
     def continuous_optimise(self, benchmark_obj: Benchmarker) -> dict[str, bool]:
         # Explores the state space until it is done (as part of an anytime algorithm)
         while self.states_explored < 2 ** len(self.current_flags.keys()):
-
-            # Get next optimisation flags
-            self.current_flags = self.optimisation_step(self.current_flags)
-
-            # Validate & benchmark next flags
-            validated_flag_choices = validate_flag_choices(self.current_flags)
-            current_time = benchmark_obj.benchmark_flag_choices(
-                opt_flag=create_flag_string(validated_flag_choices))
-
-            # Check & record improvement
-            if self.fastest_time is None or current_time < self.fastest_time:
-                self.fastest_time = current_time
-                self.fastest_flags = self.current_flags
-
-            self.states_explored += 1
+            self.optimisation_step(benchmark_obj)
 
         # Clean up class for next optimisation run before returning flags
         flags_to_return = self.fastest_flags
@@ -46,22 +35,27 @@ class RandomSearchOptimiser(FlagOptimiser):
         :return: Dictionary with optimal flag combination.
         """
         for i in range(n):
-            self.current_flags = self.optimisation_step(self.current_flags)
-
-            validated_flag_choice = validate_flag_choices(self.current_flags)
-            current_time = benchmark_obj.benchmark_flag_choices(
-                opt_flag=create_flag_string(validated_flag_choice))
-
-            if self.fastest_time is None or current_time < self.fastest_time:
-                self.fastest_time = current_time
-                self.fastest_flags = self.current_flags
-
-            self.states_explored += 1
+            self.optimisation_step(benchmark_obj)
 
         # Clean up class for next optimisation run before returning flags
         flags_to_return = self.fastest_flags
         self.clear_between_runs()
         return flags_to_return
+
+    def optimisation_step(self, benchmark_obj: Benchmarker) -> None:
+        self.current_flags = self.get_random_flags(self.current_flags)
+
+        validated_flag_choice = validate_flag_choices(self.current_flags)
+        current_time = benchmark_obj.benchmark_flag_choices(
+            opt_flag=create_flag_string(validated_flag_choice))
+
+        if self.fastest_time is None or current_time < self.fastest_time:
+            self.fastest_time = current_time
+            self.fastest_flags = self.current_flags
+
+        self.states_explored += 1
+        self.opt_steps_done += 1
+        self.print_optimisation_info()
 
 
     def clear_between_runs(self):
