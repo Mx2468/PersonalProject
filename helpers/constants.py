2cf439faca43c36ce50aac1ec418ed70da0d52f0
Maksymilian Sekula <maksymilian.sekula@hotmail.com>
2024-03-11 17:33:33 +0000

    Add constants for all flag paths

--- helpers/constants.py
+++ helpers/constants.py
@@ -1,6 +1,8 @@
 """A file containing global constant values"""
 
 COMPILED_CODE_FILE = "filetotest"
+ALL_BINARY_FLAGS_PATH = "./flags/binary_flags.txt"
+ALL_DOMAIN_FLAGS_PATH = "./flags/domain_flags.json"
 
 INTEGER_DOMAIN_UPPER_BOUND = 100
 N_BENCHMARK_RUNS = 3
