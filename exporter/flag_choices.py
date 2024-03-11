6f8919f3ceaf36d972a2ccf49723ad652a533daf
Maksymilian Sekula <maksymilian.sekula@hotmail.com>
2024-03-11 21:22:11 +0000

    make class attributes in exporter private

--- exporter/flag_choices.py
+++ exporter/flag_choices.py
@@ -5,19 +5,19 @@ class FlagChoicesExporter:
         """
         :param file_name: A path for the file to write to
         """
-        self.file = None
-        self.file_name = file_name
-        self.flags = flags
+        self._file = None
+        self._file_name = file_name
+        self._flags = flags
 
     def __enter__(self) -> 'FlagFileReader':
         """Overwrites magic method to allow easy use with "with" statements"""
-        self.file = open(self.file_name, 'w', encoding='UTF-8')
+        self._file = open(self._file_name, 'w', encoding='UTF-8')
         return self
 
     def __exit__(self, exception_type, exception_value, traceback) -> None:
         """Uses magic method to close the file after we're done with it in a "with" statement"""
-        self.file.close()
+        self._file.close()
 
     def export_flags(self):
-        print(f"Writing flag choices to {self.file_name}")
-        self.file.write(helpers.create_flag_string(self.flags))
+        print(f"Writing flag choices to {self._file_name}")
+        self._file.write(helpers.create_flag_string(self._flags))
