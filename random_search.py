46707379d09d0c54b6dac1c00596997cdd2d9231
Maksymilian Sekula <maksymilian.sekula@hotmail.com>
2024-03-13 23:49:42 +0000

    Remove redundant file

--- random_search.py
+++
@@ -1,5 +0,0 @@
-# A module to do a random exhaustive search of the flags for a compilation
-
-from random import getrandbits
-def random_search(flags: list[str]) -> dict[str, bool]:
-    return {flag: bool(getrandbits(1)) for flag in flags}
