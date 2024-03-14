8f9bdbc42391c36651b92646a82daecb84d3e652
Maksymilian Sekula <maksymilian.sekula@hotmail.com>
2024-03-14 20:29:48 +0000

    Allow for getting a `Flags` object from the `FlagConfigurationReader`

--- reader/flag_configuration_reader.py
+++ reader/flag_configuration_reader.py
@@ -1,4 +1,5 @@
 # A module to read a subset of flags with defined values
+from core.flags import Flags
 from reader.all_flags_reader import AllTypeFlagsReader
 import helpers.constants as constants
 
@@ -7,6 +8,7 @@ class FlagConfigurationReader:
         self.binary_flag_file = binary_flag_file
         self.domain_flag_file = domain_flag_file
         self.flag_configuration = {}
+        self.flags_obj = Flags()
 
     def read_in_flags(self) -> None:
         # Read flags the values are provided for
@@ -15,9 +17,11 @@ class FlagConfigurationReader:
         # Set binary flags seen to true
         for flag in reader.get_all_binary_flag_names():
             self.flag_configuration[flag] = True
+            self.flags_obj.add_flag(flag, [True, False], True)
         # Set domain flags to their "default" values
-        for flag, value in reader.get_all_domain_defaults().items():
-            self.flag_configuration[flag] = value
+        for flag ,domain in reader.get_domain_flag_domains().items():
+            self.flag_configuration[flag] = reader.domain_defaults[flag]
+            self.flags_obj.add_flag(flag, domain, reader.domain_defaults[flag])
 
         # Read all possible flags
         reader = AllTypeFlagsReader(constants.ALL_BINARY_FLAGS_PATH, constants.ALL_DOMAIN_FLAGS_PATH)
@@ -32,8 +36,15 @@ class FlagConfigurationReader:
             # For every flag not in the read ones - do false or default value.
             if flag_name in bin_flags:
                 self.flag_configuration[flag_name] = False
+                self.flags_obj.add_flag(flag_name, [True, False], False)
             else:
                 self.flag_configuration[flag_name] = all_domain_defaults[flag_name]
+                self.flags_obj.add_flag(flag_name,
+                                        reader.flag_domains[flag_name],
+                                        reader.domain_defaults[flag_name])
 
     def get_flag_configuration(self) -> dict[str, str|bool]:
         return self.flag_configuration
+
+    def get_flag_obj(self) -> Flags:
+        return self.flags_obj
