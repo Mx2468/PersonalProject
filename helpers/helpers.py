edb67725afc008e4caac885b3caf2f7c0c69c1ad
Maksymilian Sekula <maksymilian.sekula@hotmail.com>
2024-03-15 16:56:01 +0000

    Document helpers functions

--- helpers/helpers.py
+++ helpers/helpers.py
@@ -1,28 +1,41 @@
-"""
-A file with miscellaneous helper methods
-Currently only a flag string helper method
-"""
+"""A module with miscellaneous helper methods"""
 
 from random import getrandbits, choice, randint
+from typing import Any
 
 from core.flags import Flags
 from helpers.constants import INTEGER_DOMAIN_UPPER_BOUND
 
 def get_random_integer(lower_bound = 1, upper_bound = INTEGER_DOMAIN_UPPER_BOUND) -> int:
+    """
+    Get a random integer value
+
+    :param lower_bound: The lower bound (inclusive) of the integers to randomly choose from
+    :param upper_bound: The upper bound (inclusive) of the integers to randomly choose from
+    :return: A random integer from the provided range
+    """
     return randint(lower_bound, upper_bound)
 
 def get_random_flag_sample(flags: Flags) -> dict[str, bool|str]:
     """
-    Get a random sample from a set of binary flags
-    :param flags: A list of binary flags
-    :return: A dictionary of flags, along with if they were chosen or not
+    Get a random sample from a set of flags
+
+    :param flags: A `Flags` object containing the flags to randomly choose from
+    :return: A dictionary of flag names, mapped to their randomly chosen values
     """
     flag_choices = {}
     for flag_name in flags.get_all_flag_names():
         flag_choices[flag_name] = get_random_individual_flag_choice(flags, flag_name)
     return flag_choices
 
-def get_random_individual_flag_choice(flags_obj: Flags, flag_name: str):
+def get_random_individual_flag_choice(flags_obj: Flags, flag_name: str) -> str|bool|int:
+    """
+    Get a random choice for a given flag
+
+    :param flags_obj: A `Flags` object containing the given flag
+    :param flag_name: The name of the flag to obtain a random value for
+    :return: A randomly chosen value from the given flag's domain
+    """
     domain = flags_obj.get_flag_domain(flag_name)
     match domain:
         case "Integer":
@@ -50,7 +63,6 @@ def create_flag_string(flag_choices: dict[str, bool|str]) -> str:
     Creates a string of compiler flags that can be used to run the compiler
 
     :param flag_choices: The flags to be/not be used by the compiler
-
     :return: A string of compiler flags in the format to run the compilation
     """
     final_str = ""
