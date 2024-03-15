83294782b422d869ab222267bd3cbb1a341de294
Maksymilian Sekula <maksymilian.sekula@hotmail.com>
2024-03-15 16:39:19 +0000

    Document flags code properly

--- core/flags.py
+++ core/flags.py
@@ -1,6 +1,9 @@
+"""A class to represent optimisation flags"""
 from reader.all_flags_reader import AllTypeFlagsReader
 
 class Flags:
+    """A class to represent optimisation flags"""
+
     all_flag_names: list[str]
     flag_domains: dict[str, str|list[str]|list[bool]]
     domain_flag_default: dict[str, str|bool]
@@ -13,6 +16,7 @@ class Flags:
     def load_in_flags(self, binary_flag_path: str, domain_flag_path: str) -> None:
         """
         Loads in the flag info from the binary and domain flag paths provided
+
         :param binary_flag_path: The path for information about the binary flags (a .txt file)
         :param domain_flag_path: The path for information about the domain flags (a .json file)
         """
@@ -23,7 +27,11 @@ class Flags:
         self.domain_flag_default = reader.get_all_domain_defaults()
 
     def remove_flag(self, flag_name: str) -> None:
-        """ Remove a flag from the object"""
+        """
+        Removes a flag from the object
+
+        :param flag_name: The name of the flag to remove from this object
+        """
         if flag_name in self.all_flag_names:
             self.all_flag_names.remove(flag_name)
             if flag_name in self.flag_domains.keys():
@@ -31,6 +39,13 @@ class Flags:
                 del self.domain_flag_default[flag_name]
 
     def add_flag(self, flag_name: str, domain, default_value) -> None:
+        """
+        Adds a flag to the object
+
+        :param flag_name: The name of the flag to add
+        :param domain: The domain of possible values for the flag
+        :param default_value: The default/desired value for the flag
+        """
         if not(flag_name in self.all_flag_names):
             self.all_flag_names.append(flag_name)
 
@@ -41,20 +56,43 @@ class Flags:
             self.domain_flag_default[flag_name] = default_value
 
     def get_all_flag_names(self) -> list[str]:
-        """Returns a list of all the flag names"""
+        """
+        Returns a list of all the flag names
+
+        :return: A list of all the flag names present in the object
+        """
         return self.all_flag_names
 
     def get_domain_flag_defaults(self) -> dict[str, str]:
-        """Returns a dictionary mapping each flag name to its default value"""
+        """
+        Returns a dictionary mapping each domain flag name to its default value
+
+        :return: A dictionary mapping the domain flag to its default value
+        """
         return self.domain_flag_default
 
     def get_all_flag_domains(self) -> dict[str, str|list[str]|list[bool]]:
-        """Returns a dictionary mapping all flag names to the domains that can be applied to them"""
+        """
+        Returns a dictionary mapping all flag names to the domains that can be applied to them
+
+        :return: A dictionary mapping each flag name to its domain
+        """
         return self.flag_domains
 
     def get_flag_domain(self, flag_name: str) -> str|list[str]|list[bool]:
-        """Returns a domain given a specific flag name"""
+        """
+        Returns a domain given a specific flag name
+
+        :param flag_name: The name of the flag to get the domain for
+        :return: The domain for the flag provided
+        """
         return self.flag_domains[flag_name]
 
     def get_flag_default(self, flag_name: str) -> str|bool:
+        """
+        Returns the default value for a given flag name
+
+        :param flag_name: The name of the flag to get the default value for
+        :return: The default value for the flag provided
+        """
         return self.domain_flag_default[flag_name]
