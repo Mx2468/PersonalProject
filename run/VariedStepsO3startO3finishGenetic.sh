f8c514e4ed3dbd1472ecfc20f9085c0d3e08ff72
Maksymilian Sekula <maksymilian.sekula@hotmail.com>
2024-04-12 01:11:37 +0100

    Change line separators on run files

--- run/VariedStepsO3startO3finishGenetic.sh
+++ run/VariedStepsO3startO3finishGenetic.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-conda run --live-stream -n PersonalProject python flag_controller.py -i ./test_cases/BenchmarksGame-fannkuchredux5.cpp -m genetic -n 250 --num-code-runs 5 --log-results &&
-conda run --live-stream -n PersonalProject python flag_controller.py -i ./test_cases/BenchmarksGame-fannkuchredux5.cpp -m genetic -n 500 --num-code-runs 5 --log-results &&
-conda run --live-stream -n PersonalProject python flag_controller.py -i ./test_cases/BenchmarksGame-fannkuchredux5.cpp -m genetic -n 750 --num-code-runs 5 --log-results &&
-conda run --live-stream -n PersonalProject python flag_controller.py -i ./test_cases/BenchmarksGame-fannkuchredux5.cpp -m genetic -n 1000 --num-code-runs 5 --log-results
+conda run --live-stream -n PersonalProject python flag_controller.py -i ./test_cases/BenchmarksGame-fannkuchredux5.cpp -m genetic -n 25 --num-code-runs 5 --log-results &&
+conda run --live-stream -n PersonalProject python flag_controller.py -i ./test_cases/BenchmarksGame-fannkuchredux5.cpp -m genetic -n 50 --num-code-runs 5 --log-results &&
+conda run --live-stream -n PersonalProject python flag_controller.py -i ./test_cases/BenchmarksGame-fannkuchredux5.cpp -m genetic -n 75 --num-code-runs 5 --log-results
+conda run --live-stream -n PersonalProject python flag_controller.py -i ./test_cases/BenchmarksGame-fannkuchredux5.cpp -m genetic -n 100 --num-code-runs 5 --log-results
