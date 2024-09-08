a2d6fd5344affe6b51f5fb982d5bb3990df23a0a
Maksymilian Sekula <maksymilian.sekula@hotmail.com>
2024-09-08 18:32:15 +0100

    Tweak optimisation parameters

--- optimisers/config/gaussian_process_config.py
+++ optimisers/config/gaussian_process_config.py
@@ -1,10 +1,10 @@
 # Number of points to sample before gaussian process estimator starts estimating
 # the best points to select. Default is 103 due to LHS being used as default for sampling
 # for shorter runs, the value will need to be reduced
-N_INITIAL_POINTS = 103
+N_INITIAL_POINTS = 12
 
 # Generator of initial set of random points - default set to latin hypercube
-INITIAL_POINT_GENERATOR_METHOD = "lhs"
+INITIAL_POINT_GENERATOR_METHOD = "random"
 
 # Acquisition function to use
 # default value "gp_hedge" chooses one of 3 functions each iteration
